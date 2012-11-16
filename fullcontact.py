c6c76cd51c0c4d6a2ede12fd7df5f69683d9e72a
patrick-russell <prussell@gmail.com>
2012-11-16 08:39:23 -0700

    added support for html and other formats
    Added logic to handle html formatted responses (it'll handle XML too).
    Also cleaned up to be more pep8

--- fullcontact.py
+++ fullcontact.py
@@ -14,32 +14,34 @@ CLIENT_VERSION = '0.2'
 USER_AGENT = 'Python 2.7.3/Fullcontact Python module version{0}'.format(CLIENT_VERSION)
 
 #initialize logging.
-logging.basicConfig(filename='fullcontact.log',level=logging.INFO,format='%(asctime)s %(message)s')
+logging.basicConfig(filename='fullcontact.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
 
 class fullContactError():
     pass
-   
+
+
 class nameStats(object):
     '''look up first and last name via fullcontact's free name api'''
-    
-    def __init__(self,api_key):
+
+    def __init__(self, api_key):
         self.api_key = api_key
-        
-    def lookup_request(self,method,lkup={}):
+
+    def lookup_request(self, method, lkup={}):
         '''method to build the url, make the request, and dump the json to a dict'''
-        
+
         #ensure proper encoding for look up values
         params = {}
-        for k,v in lkup.iteritems(): 
-            params[k] = unicode(v).encode('utf-8')           
+        for k, v in lkup.iteritems():
+            params[k] = unicode(v).encode('utf-8')
 
         params['apiKey'] = self.api_key
         data = urlencode(params)
-       
+
         #make the url,create the request and set user agent
-        lookup_url = "{0}{1}?{2}".format(END_POINT,method,data)                   
-        lookup_req = Request(url = lookup_url, headers = {'User-agent': USER_AGENT})
-        
+        lookup_url = "{0}{1}?{2}".format(END_POINT, method, data)
+        lookup_req = Request(url=lookup_url, headers={'User-agent': USER_AGENT})
+
         logging.info('url created ' + lookup_req.get_full_url())
         logging.info(str(lkup))
 
@@ -49,36 +51,37 @@ class nameStats(object):
             logging.error(e.code)
             raise fullContactError()
         else:
-            name_data = load(lookup)            
-            logging.info('status code = '+str(name_data['status'])+' '+str(lkup))       
+            name_data = load(lookup)
+            logging.info('status code = ' + str(name_data['status']) + ' ' + str(lkup))
             return name_data
 
-    def lookitup(self,method = 'name/stats.json',fname='', lname=''):
-        return self.lookup_request(method,lkup={'givenName':fname,'familyName':lname})
-        
+    def lookitup(self, method='name/stats.json', fname='', lname=''):
+        return self.lookup_request(method, lkup={'givenName': fname, 'familyName': lname})
+
+
 class personLookup(object):
     '''look up person by email/phone/twitter via fullcontact's paid person api'''
 
-    def __init__(self,api_key):
+    def __init__(self, api_key):
         self.api_key = api_key
 
-    def lookup_request(self,method,lkup):
-        '''method to build up the url, make the request, and dump the json to a dict
-           valid lookup types are email,twitter,phone,facebookUsername'''
-        
+    def lookup_request(self, method, lkup):
+        '''method to build up the url, make the request, and, if using JSON dump
+          the JSON to a dict valid lookup types are email,twitter,phone,facebookUsername'''
+
         #ensure proper encoding for look up values
         params = {}
-        for k,v in lkup.iteritems():
+        for k, v in lkup.iteritems():
             params[k] = unicode(v).encode('utf-8')
-            
+
         params['apiKey'] = self.api_key
         data = urlencode(params)
-        
+
         #make the url,create the request and set user agent
-        lookup_url = '{0}{1}?{2}'.format(END_POINT,method,data)
-        lookup_req = Request(url = lookup_url, headers = {'User-agent': USER_AGENT})
-        
-        logging.info('url created '+ lookup_req.get_full_url())
+        lookup_url = '{0}{1}?{2}'.format(END_POINT, method, data)
+        lookup_req = Request(url=lookup_url, headers={'User-agent': USER_AGENT})
+
+        logging.info('url created ' + lookup_req.get_full_url())
         logging.info(str(lkup))
 
         try:
@@ -87,9 +90,15 @@ class personLookup(object):
             logging.error(e.code)
             raise fullContactError()
         else:
-            person_data = load(lookup)            
-            logging.info('status code = '+str(person_data['status'])+' '+str(lkup))       
+            if method == 'person.json':
+                person_data = load(lookup)
+                logging.info('status code = ' + str(person_data['status']) + ' ' + str(lkup))
+            elif method == 'person.html':
+                person_data = lookup.read().decode('utf-8')  # TODO need logging
+            else:
+                person_data = lookup
+            # logging.info('status code = '+str(person_data['status'])+' '+str(lkup)) #TODO
             return person_data
-        
-    def lookitup(self, lkup_value,lkup_type='email',method='person.json'):
-        return self.lookup_request(method,lkup={lkup_type:lkup_value})
+
+    def lookitup(self, lkup_value, lkup_type='email', method='person.json'):
+        return self.lookup_request(method, lkup={lkup_type: lkup_value})

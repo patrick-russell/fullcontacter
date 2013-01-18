7d4f61078cca533f387eafe6f21bfe3b3bccb1af
patrick-russell <prussell@gmail.com>
2013-01-18 14:12:52 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='fullcontacter',
+      version='0.5',
+      description="Library for interacting with Fullcontact's APIs.",
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      keywords='contact information fullcontact',
+      url='http://github.com/patrick-russell/fullcontacter',
+      author='Patrick Russell',
+      author_email='prussell@gmail.com',
+      license='MIT',
+      packages=['fullcontacter'],
+      zip_safe=False)
